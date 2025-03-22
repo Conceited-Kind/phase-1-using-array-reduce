d5382390820f50a2282cafaa960ff47262f2271f
Allan Kinuva <allankinuva@gmail.com>
2025-03-22 11:03:27 +0300

    using-array-reduce

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Calculate the total number of batteries
+const totalBatteries = batteryBatches.reduce((sum, batch) => sum + batch, 0);
+
+console.log(totalBatteries); // Optional: To verify the result
